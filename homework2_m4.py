ccd30e51f5983e9037541ba9dd16b299908b9052
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-21 02:29:54 +0200

    homework2

---
+++ homework2_m4.py
@@ -0,0 +1,14 @@
+
+path = input("Enter the path to the file with cats inormation: ") 
+
+def get_cats_info(path):
+    cat_list = []
+    with open(path, "r", encoding='utf-8') as fh:
+        for line in fh:
+            id, name, age = line.strip().split(",")
+            cat_dict = {"id": id, "name": name, "age": age}
+            cat_list.append(cat_dict)
+    return cat_list
+
+cats_info = get_cats_info(path)
+print(cats_info)
