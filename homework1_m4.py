b6d7b209674cf413023f9e4bfd2c96271fe12486
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-25 20:22:59 +0200

    Correction

--- homework1_m4.py
+++ homework1_m4.py
@@ -9,20 +9,21 @@ def total_salary(path): # calculation of the total amount of employees' salaries
             name, salary = line.split(",") # split the line into name and salary
             total += int(salary)
             count += 1
-            average = int(total / count)
+        if count == 0:
+           return 0, 0
+        average = total // count
         return total, average # return total and average salary
 
 try: # try to calculate the total amount of employees' salaries and its average value
-    total, average = total_salary(path)
-except FileNotFoundError: # if the file is not found
-    print(f"File not found: {path}")
-except PermissionError: # if the file cannot be read
-    print(f"Permission denied when reading file: {path}")
-except IsADirectoryError: # if the path is a directory
-    print(f"Path is a directory, expected a file: {path}")
+   total, average = total_salary(path)
 except UnicodeDecodeError: # if the file cannot be decoded
-    print(f"Cannot decode file (unexpected encoding): {path}")
+    print(f"Файл не в форматі UTF-8: {path}")
 except OSError as e: # if there is an error reading the file
-    print(f"Error reading file {path}: {e}")
+    print(f"Помилка читання файлу {path}: {e}")
+except ValueError: # if the salary is not a number
+    print(f"Файл не відповідає шаблону \"ім'я, заробітна плата\": {path}")
 else:
-    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+     if total == 0:
+        print("Файл не містить інфіормацію про заробітну плату")
+     else:
+        print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
